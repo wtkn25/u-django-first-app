2b51279ba9d9ae819e0bc4f96dd0d24cdbcb2ce2
wtkn25 <atrastar72@gmail.com>
2021-08-14 16:44:49 +0900

    URLディスパッチ

--- first_app/views.py
+++ first_app/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hello")
+
+
+def user_page(request, user_name):
+    return HttpResponse(f"<h1>{user_name}'s page<h1>")
+
+
+def number_page(request, number):
+    return HttpResponse(f"<h1>page_number = {number}<h1>")
